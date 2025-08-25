35f4e2c3c06c8d87cbc4d19612a4132de4b7db47
Felix Ledung <felix.ledung@learnet.se>
2025-08-25 14:27:47 +0200

    Deploy

--- lesson4_25_august/task10.py
+++ lesson4_25_august/task10.py
@@ -3,12 +3,12 @@ import turtle
 s = turtle.getscreen()
 t = turtle.Turtle()
 
-s._color("yellow")
+s.bgcolor("yellow") #bakgrundsfärgen gul
 
-t.color("pink")
-t.pensize(10)
+t.color("pink") #border runt cirkeln. Färgen är rosa.
+t.pensize(10) #Storleken är 10px
 
-t.fillcolor("cyan") 
+t.fillcolor("cyan") #cirkelns bakgrundsfärg är cyan
 t.begin_fill()
 t.circle(100)
 t.end_fill()
