97f3f4d66d3363b448c5ea2bf101bf3a219a5d92
Felix Ledung <mrfelix880@gmail.com>
2025-09-10 22:52:36 +0200

    Evening deploy

--- lesson10_10_september_workshop/step1_task1.py
+++ lesson10_10_september_workshop/step1_task1.py
@@ -3,7 +3,10 @@ import turtle
 s = turtle.getscreen()
 t = turtle.Turtle()
 
-t.speed(5)
+t.speed(0)
+
+t.pensize(2.5)
+t.pencolor('black')
 
 radius1 = 150
 radius2 = 95
@@ -34,6 +37,31 @@ t.circle(radius3)
 t.color('red')
 t.end_fill()
 
+t.setheading(180)
+t.penup()
+t.begin_fill()
+t.goto(0, 210)
+t.color('#2e2e2e')
+t.pendown()
+t.forward(100)
+t.right(90)
+t.forward(25)
+t.left(90)
+
+t.forward(100)
+t.left(90)
+t.forward(25)
+t.right(90)
+
+t.forward(100)
+t.left(90)
+t.forward(25)
+t.right(90)
+
+t.backward(300)
+t.left(90)
+t.forward(25)
+
 t.penup()
 t.begin_fill()
 t.goto(0,130)
@@ -58,8 +86,39 @@ t.circle(radiusVerySmall)
 t.color('white')
 t.end_fill()
 
+t.pensize(5)
+t.pencolor('black')
+
 t.penup()
 t.begin_fill()
-t.goto()
+t.goto(0,25)
+t.pendown()
+t.circle(radiusVerySmall)
+t.color('white')
+t.end_fill()
+
+t.pensize(5)
+t.pencolor('black')
+
+t.penup()
+t.begin_fill()
+t.goto(0,50)
+t.pendown()
+t.circle(radiusVerySmall)
+t.color('white')
+t.end_fill()
+
+t.pensize(5)
+t.pencolor('black')
+
+t.penup()
+t.begin_fill()
+t.goto(0,75)
+t.pendown()
+t.circle(radiusVerySmall)
+t.color('white')
+t.end_fill()
+
+t.hideturtle()
 
 turtle.done()
